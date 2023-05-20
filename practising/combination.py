d01df6b9a617ce8ddd6034bd646eb147817a1f7f
Ioannis <giandabdouk@gmail.com>
2023-05-20 10:48:51 +0100

    Tried combinations of models

--- practising/combination.py
+++ practising/combination.py
@@ -22,29 +22,84 @@ def symbols_from_market(industry, number_of_symbols):
 
     return stock_list
 
-#Gets peg ratios of products and saves 1/peg for each in a list (so that higher is better)
+#Gets  ratios of products and saves 1/ratio for each in a list (so that higher is better)
 
-def peg_ratios_list(stock_list):
+def ratios_inverted(stock_list):
 
     peg_ratios = []
+    pe_ratios = []
+    equity_to_debt = []
 
     for i in stock_list:
 
         stock = Ticker(i)
 
         peg_ratios.append(1/stock.valuation_measures["PegRatio"][0])
+        pe_ratios.append(1/stock.valuation_measures["PeRatio"][0])
+        
+        debt = stock.balance_sheet()["TotalDebt"][0]
+        equity = stock.balance_sheet()["StockholdersEquity"][0]
 
-    return peg_ratios
+        equity_to_debt.append(equity/debt)
 
 
+    return peg_ratios,pe_ratios,equity_to_debt
 
-#### TESTING ####
 
-stocks = symbols_from_market("semiconductors",15)
-pegs = peg_ratios_list(stocks)
+# Uses a MA to return a decision on whether there is a trend or not
 
-print(stocks)
-print(pegs)
+def current_trend(stock_list):
+
+    trends = []
+
+    for i in stock_list:
+
+        stock = Ticker(i)
+
+        hist = stock.history(period = "3mo")
+
+        closing_prices = hist["close"]
+
+
+        # Identify ten and fifty day averages
+        ten_day_ave = closing_prices[50::].mean()
+
+        fifty_day_ave = closing_prices[10::].mean()
+
+        if ten_day_ave > fifty_day_ave:
+
+            trends.append(0.5)
+
+        else:
+
+            trends.append(-0.5)
+
+    return trends
+
+
+# Getting score for each product
+
+stocks = symbols_from_market("aerospace_defense",10)
+pegs,pes,ed = ratios_inverted(stocks)
+trends = current_trend(stocks)
+
+scores  = []
+for i in stocks:
+
+    index = stocks.index(i)
+
+    score = pegs[index] + pes[index] + 0.1*ed[index] + trends[index]
+
+    scores.append([i , score])
+
+
+#sort scores by highest recommended    
+
+def sorted(score):
+    return score[1]
+
+scores.sort(reverse = True ,  key = sorted)
+print(scores)
 
 
 
